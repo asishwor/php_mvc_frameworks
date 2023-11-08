ec8845645aa8f4642cd7ef4a08e71cb7b6422938
ishwor khadka <asishwor@gmail.com>
2023-11-08 09:15:29 +0545

    update MVC frameworks

--- public/index.php
+++ public/index.php
@@ -1,17 +1,21 @@
 <?php 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use app\controller\SiteController;
 use app\core\Application;
 
 $app = new Application( dirname( __DIR__ ));
+$siteController = new SiteController();
 
-$app->router->get('/', 'home');
+$app->router->get('/', [ $siteController, 'home' ]);
 
-$app->router->get('/contact', 'contact');
+$app->router->get('/contact', [ $siteController, 'contact' ]);
+$app->router->post('/contact', [ $siteController, 'contactHandle' ]);
 
 $app->router->get(
     '/about',
-    function () {
+    function () 
+    {
         return 'About';
     }
 );
